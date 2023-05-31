c10b4db65270f348839e54ec45d85b8632b89b49
Allard Buijze <allard.buijze@axoniq.io>
2023-05-31 14:52:24 +0200

    Added solution to lab 1

--- rental/src/main/java/io/axoniq/demo/bikerental/rental/query/BikeStatusProjection.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/rental/query/BikeStatusProjection.java
@@ -1,16 +1,100 @@
 package io.axoniq.demo.bikerental.rental.query;
 
+import io.axoniq.demo.bikerental.coreapi.rental.BikeInUseEvent;
+import io.axoniq.demo.bikerental.coreapi.rental.BikeRegisteredEvent;
+import io.axoniq.demo.bikerental.coreapi.rental.BikeRequestedEvent;
+import io.axoniq.demo.bikerental.coreapi.rental.BikeReturnedEvent;
+import io.axoniq.demo.bikerental.coreapi.rental.BikeStatus;
+import io.axoniq.demo.bikerental.coreapi.rental.RentalStatus;
+import io.axoniq.demo.bikerental.coreapi.rental.RequestRejectedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BikeStatusProjection {
 
     private final BikeStatusRepository bikeStatusRepository;
+    private final QueryUpdateEmitter updateEmitter;
 
-    public BikeStatusProjection(BikeStatusRepository bikeStatusRepository) {
+    public BikeStatusProjection(BikeStatusRepository bikeStatusRepository, QueryUpdateEmitter updateEmitter) {
         this.bikeStatusRepository = bikeStatusRepository;
+        this.updateEmitter = updateEmitter;
     }
 
-    // TODO Implement relevant Query and Event Handlers
+    @EventHandler
+    public void on(BikeRegisteredEvent event) {
+        var bikeStatus = new BikeStatus(event.getBikeId(), event.getBikeType(), event.getLocation());
+        bikeStatusRepository.save(bikeStatus);
+        updateEmitter.emit(q -> "findAll".equals(q.getQueryName()), bikeStatus);
+    }
 
+    @EventHandler
+    public void on(BikeRequestedEvent event) {
+        bikeStatusRepository.findById(event.getBikeId())
+                            .map(bs -> {
+                                bs.requestedBy(event.getRenter());
+                                return bs;
+                            })
+                            .ifPresent(bs -> {
+                                updateEmitter.emit(q -> "findAll".equals(q.getQueryName()), bs);
+                                updateEmitter.emit(String.class, event.getBikeId()::equals, bs);
+                            });
+    }
+
+    @EventHandler
+    public void on(BikeInUseEvent event) {
+        bikeStatusRepository.findById(event.getBikeId())
+                            .map(bs -> {
+                                bs.rentedBy(event.getRenter());
+                                return bs;
+                            })
+                            .ifPresent(bs -> {
+                                updateEmitter.emit(q -> "findAll".equals(q.getQueryName()), bs);
+                                updateEmitter.emit(String.class, event.getBikeId()::equals, bs);
+                            });
+    }
+
+    @EventHandler
+    public void on(BikeReturnedEvent event) {
+        bikeStatusRepository.findById(event.getBikeId())
+                            .map(bs -> {
+                                bs.returnedAt(event.getLocation());
+                                return bs;
+                            })
+                            .ifPresent(bs -> {
+                                updateEmitter.emit(q -> "findAll".equals(q.getQueryName()), bs);
+                                updateEmitter.emit(String.class, event.getBikeId()::equals, bs);
+                            });
+
+    }
+
+    @EventHandler
+    public void on(RequestRejectedEvent event) {
+        bikeStatusRepository.findById(event.getBikeId())
+                            .map(bs -> {
+                                bs.returnedAt(bs.getLocation());
+                                return bs;
+                            })
+                            .ifPresent(bs -> {
+                                updateEmitter.emit(q -> "findAll".equals(q.getQueryName()), bs);
+                                updateEmitter.emit(String.class, event.getBikeId()::equals, bs);
+                            });
+    }
+
+    @QueryHandler(queryName = "findAll")
+    public Iterable<BikeStatus> findAll() {
+        return bikeStatusRepository.findAll();
+    }
+
+    @QueryHandler(queryName = "findAvailable")
+    public Iterable<BikeStatus> findAvailable(String bikeType) {
+        return bikeStatusRepository.findAllByBikeTypeAndStatus(bikeType, RentalStatus.AVAILABLE);
+    }
+
+    @QueryHandler(queryName = "findOne")
+    public BikeStatus findOne(String bikeId) {
+        return bikeStatusRepository.findById(bikeId).orElse(null);
+    }
 }
