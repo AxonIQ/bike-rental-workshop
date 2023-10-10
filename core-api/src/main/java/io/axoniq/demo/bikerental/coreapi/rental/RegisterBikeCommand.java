f486d7c64a3b957a87d8ba71582fb25548eb4b07
Allard Buijze <allard.buijze@axoniq.io>
2023-10-10 10:07:46 +0200

    Fixed typo

--- core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/rental/RegisterBikeCommand.java
+++ core-api/src/main/java/io/axoniq/demo/bikerental/coreapi/rental/RegisterBikeCommand.java
@@ -5,7 +5,7 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier;
 public record RegisterBikeCommand(
         @TargetAggregateIdentifier String bikeId,
         String bikeType,
-        String locationg
+        String location
 ) {
 
 }
